63e4d2711501eccf9c2734284544e5f38057a61a
camalzadeelvin <camalzadeelvin@STHQ0118-01>
2025-01-30 15:54:07 +0400

    Add project files.

---
+++ ECommerceApp.UI/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using ECommerceApp.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _productService.GetAllAsync();
+            return Ok(items);
+        }
+    }
+}
---
+++ ECommerceApp.UI/Program.cs
@@ -0,0 +1,44 @@
+using ECommerce.Entities.Models;
+using ECommerceApp.Business.Abstract;
+using ECommerceApp.Business.Concrete;
+using ECommerceApp.DataAccess.Abstraction;
+using ECommerceApp.DataAccess.Concrete.EfEntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var conn = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<NorthwindContext>(options =>
+{
+    options.UseSqlServer(conn);
+});
+
+builder.Services.AddScoped<IProductDal, EfProductDal>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
